61fb1a9b68180770f68e1320c71c6c54a9f2f13c
artlostintime <artlostintime@gmail.com>
2024-02-02 11:07:26 +0530

    NA-2

--- install.sh
+++ install.sh
@@ -6,9 +6,9 @@ echo "Hello, World!"
 
 
 
-echo "Hello, World!"
+# echo "Hello, World!"
 
-# replace with the URL of the script you want to download and execute
-url="https://raw.githubusercontent.com/artlostintime/i-hate-setting-up-new-distro-everyday/main/install.sh"
+# # replace with the URL of the script you want to download and execute
+# url="https://raw.githubusercontent.com/artlostintime/i-hate-setting-up-new-distro-everyday/main/install.sh"
 
-bash <(curl -s $url)
+# bash <(curl -s $url)
